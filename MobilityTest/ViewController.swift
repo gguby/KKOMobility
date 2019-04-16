c82569ba67f013611374c271b07209400ec63825
정원식(Toad) <wonsik.jung@iriver.com>
2019-04-16 11:33:20 +0900

    Initial Commit

---
+++ MobilityTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobilityTest
+//
+//  Created by wsjung on 16/04/2019.
+//  Copyright © 2019 wsjung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
